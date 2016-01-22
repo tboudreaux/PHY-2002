ec0080b38b45b6efa47de102c2183993da97056c
Thomas Boudreaux <thomas@boudreauxmail.com>
2016-01-22 12:57:09 -0500

    Cleaned Up code quite a lot made some small aesthetic imporovements and added chart labels, stll have to work on getting line plots and not scatter plots

--- BPlot.py
+++ BPlot.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import platform
 import sys
 import os
-from PyQt4 import QtGui, QtCore
+from PyQt4 import QtGui
 from basicgui import Ui_Header
 
 operatings = platform.system()
@@ -37,11 +37,12 @@ class Plotter(object):
             stackNum = num
         wavearray = []
         fluxarray = []
+        namearray = []
         ords = 62
+        multi = 1.5
         for i in range(stackNum):
             name = pathArray[i]
             name = name[:-1]
-            print name
             sp = fits.open(name)
             hdu = sp[0].header
 
@@ -49,18 +50,24 @@ class Plotter(object):
 
             dateobs = (hdu['DATE-OBS'])
             objName = (hdu['OBJECT'])
+            namearray.append(objName)
             wavelength = np.float64(sp[0].data[ordernum, :, 0])
             flux = np.float64(sp[0].data[ordernum, :, 1])
 
             wavearray.append(wavelength)
-            fluxarray.append(flux)
+            fluxarray.append(flux*multi)
+            multi += .1
 
         for i in range(stackNum):
             tempWave = wavearray
             tempWave = tempWave[i*ords:(i*ords)+ords]
             tempFlux = fluxarray
             tempFlux = tempFlux[i*ords:(i*ords)+ords]
-            plt.scatter(tempWave, tempFlux, label=objName)
+            plt.plot(tempWave, tempFlux)
+            plt.xlabel('Wavelength (Angstroms)')
+            plt.ylabel('Flux')
+            number = len(namearray)
+            plt.title('Single Order 1-D Spectra for ' + str(number) + ' Stars | Order number: ' + str(start))
         plt.show()
 
     @staticmethod
@@ -110,12 +117,10 @@ class MyForm(QtGui.QMainWindow):
     @staticmethod
     def stackinput():
         useArray[0] = not useArray[0]
-        print useArray
 
     @staticmethod
     def allinput():
         useArray[1] = not useArray[1]
-        print useArray
 
     def search(self):
         namearray = []
@@ -130,7 +135,6 @@ class MyForm(QtGui.QMainWindow):
                         namearray.append(objname)
         for i in range(len(namearray)):
             starname = namearray[i]
-            print starname, 'StarName'
             nameforfile = 'PathTo' + starname
             printlist = open(nameforfile, 'w')
             for root, dirs, files in os.walk('.', topdown=True):
